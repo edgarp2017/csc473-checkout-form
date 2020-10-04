635b0a8d85d9eebd2dc7e07b55050dae552fb47c
Edgar <epineda0313@gmail.com>
2020-10-03 20:23:35 -0400

    Added items to cart and enable/disables reqiured attribute when shipping fields are shown

--- checkout.js
+++ checkout.js
@@ -1,9 +1,28 @@
 function shippingForm() {
   var checkBox = document.getElementById("shippingAddress");
-  var text = document.getElementById("shippingForm");
+  var form = document.getElementById("shippingForm");
+  var first_name = document.getElementById("shipping-first-name");
+  var last_name = document.getElementById("shipping-last-name");
+  var shipping_address = document.getElementById("shipping-address");
+  var shipping_city = document.getElementById("shipping-city");
+  var shipping_state = document.getElementById("shipping-state");
+  var shipping_zip = document.getElementById("shipping-zip");
+
   if (checkBox.checked == false) {
-    text.style.display = "block";
+    form.style.display = "block";
+    first_name.required = true;
+    last_name.required = true;
+    shipping_address.required = true;
+    shipping_city.required = true;
+    shipping_state.required = true;
+    shipping_zip.required = true;
   } else {
-    text.style.display = "none";
+    form.style.display = "none";
+    first_name.required = false;
+    last_name.required = false;
+    shipping_address.required = false;
+    shipping_city.required = false;
+    shipping_state.required = false;
+    shipping_zip.required = false;
   }
 }
